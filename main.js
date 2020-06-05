e08a85c8091dad377dfba3fba857f496722c0ca9
Felix Fernandez <fernandezfelix460.gmail.com>
2020-06-05 13:21:24 -0400

    set up local storage to get books, add books and remove books from it

--- main.js
+++ main.js
@@ -67,7 +67,32 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+  static getBooks(){
+    let books;
+    if(localStorage.getItem('books') === null){
+        books =[]
+    }else{
+        books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books
+  }
+  
+ static addBook(book){
+    const books = Store.getBooks();
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+  static removeBook(isbn){
+    const books = Store.getBooks();
+    books.forEach((book, index) =>{
+        if(book.isbn === isbn){
+            books.splice(index, 1);
+        }
+    });
 
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
 // Event to Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // Event to Add a Book
